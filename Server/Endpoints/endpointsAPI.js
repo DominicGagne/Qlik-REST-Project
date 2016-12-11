e6c4a6fc5a02ea7136d6293a973dc898831c29c9
Dominic Gagne <dominicgagne@Dominics-MacBook-Pro-3.local>
2016-12-11 15:01:35 -0500

    Removed some more example endpoints from my boilerplate example in endpointsAPI.js

--- Server/Endpoints/endpointsAPI.js
+++ Server/Endpoints/endpointsAPI.js
@@ -5,7 +5,7 @@
 var endpointsAPI  = function(app, database, rootDir) {
          
     app.get('/', function (req, res) {
-        res.sendFile(rootDir + '/Public/index.html');
+        res.sendFile(rootDir + '../Public/index.html');
     });   
 
     app.post('/register', function(req, res) {
@@ -16,52 +16,6 @@ var endpointsAPI  = function(app, database, rootDir) {
             registerUser(res, req);
         }
     }); 
-
-    app.get('/test', function(req, res) {
-        res.status(200).send("hello");
-    });
-    
-
-    app.get('/protected', function(req, res) {
-        res.status(200).send("<center><h3>You have been authorized and are viewing a proctected page.</h3></center>");
-    });   
-      
-    //consider putting these helper functions into a module.
-    
-    function registerUser(res, req) {
-        var username = req.body.username;
-        var password = req.body.password;
-        database.insertOrUpdate("INSERT INTO User (Username, Password) VALUES (?, ?)", [username, passwordHash.generate(password)], function(err, userInsertID) {
-            if(err) {
-                res.status(503).send("Database insert failure.");
-            } else {
-                //here we pass in an object containing user information to serialize to their session.
-                //in practice, a database call with all pertinent user info would be used here instead.
-
-                getNewUserInfo(username, req, res, serializeNewUser);
-            } 
-        });  
-    }
-
-    
-    function getNewUserInfo(username, req, res, callback) {
-        database.fetchFirst("SELECT * FROM User WHERE User.Username = ?", [username], function (userRecord) {
-            return callback(userRecord, req, res);
-        });
-    }
-
-    function serializeNewUser(userRecord, req, res) {
-        req.login(userRecord, function(err) {
-            if(err) {
-                //unknown error, display error to console and die.
-                console.log("Fatal error in login serialization: ");
-                throw err;
-            } else {
-                //success, user is serialized.
-                res.redirect('/protected');
-            }
-        });
-    }
 };
 
 //allow this entire file  to be exported to application.js 
