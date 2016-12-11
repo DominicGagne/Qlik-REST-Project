d52509f3b59ed4ea6ff352bee0aa74ab17108a37
Dominic Gagne <dominicgagne@Dominics-MacBook-Pro-3.local>
2016-12-11 18:05:41 -0500

    POST /message endpoint has base structure, stub of function to addNewUser has been created.

--- Server/Endpoints/endpointsAPI.js
+++ Server/Endpoints/endpointsAPI.js
@@ -8,14 +8,29 @@ var endpointsAPI  = function(app, database, rootDir) {
         res.sendFile(rootDir + '../Public/index.html');
     });   
 
-    app.post('/register', function(req, res) {
-        if(!req.body.username || !req.body.password) {
-            res.status(400).send("Badly formatted request.");
+    app.post('/message', function(req, res) {
+        console.log(req.body);
+        if(! req.body.username || ! req.body.message) {
+            res.status(400).send("Badly formatted request. Missing username or message.");
         } else {
             //request is properly formatted and contains all required info.
-            registerUser(res, req);
+            console.log("username: ", req.body.username, " message: ", req.body.message);
+            database.insertOrUpdate("INSERT INTO Message (UserID, Contents) VALUES ((SELECT UserID FROM User WHERE Username = ?),?)", [req.body.username, req.body.message], function(err, insertID) {
+                //console.log("Err: ", err, " insertID: ", insertID);
+                if(err) {
+                    console.log("User does not yet exist.");
+                    addNewUser(req.body.username, req.body.message);
+                } else {
+                    console.log("User existed, success.");
+                }
+                res.status(200).send();
+            });
         }
     }); 
+
+    function addNewUser(username, message) {
+        
+    }
 };
 
 //allow this entire file  to be exported to application.js 
