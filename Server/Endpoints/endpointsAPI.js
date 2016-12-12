19fb7578a02a07b4e5a2589ea9acec87d4f00994
Dominic Gagne <dominicgagne@Dominics-MacBook-Pro-3.local>
2016-12-11 23:10:37 -0500

    Finalized palindrome endpoint GET/messageID/palindrome, as well as associated documentation.

--- Server/Endpoints/endpointsAPI.js
+++ Server/Endpoints/endpointsAPI.js
@@ -4,7 +4,9 @@
 
 var endpointsAPI  = function(app, database, rootDir) {
 
-    var palindromeModule = require('../HelperModules/palindromeModule.js');
+    var palindromeFile = require('../HelperModules/palindromeModule.js');
+    var palindromeModule = new palindromeFile();
+    console.log(palindromeModule.isPalindrome);
          
     app.get('/', function (req, res) {
         res.sendFile(rootDir + '../Public/index.html');
@@ -119,6 +121,11 @@ var endpointsAPI  = function(app, database, rootDir) {
     });
 
 
+    /**
+     * [API endpoint for GET /messages/messageID/palindrome.]
+     * Params: The MessageID of the message to evaluate as a palindrome.
+     * @return {[HTTP 200 on success, HTTP 400 on badly submitted MessageID, and HTTP 404 on no MessageID found.]}
+     */
     app.get('/messages/:messageID/palindrome', function(req, res) {
         //regex to match a string that only contains digits. Will immediately filter out bad/malicious input.
         if(req.params.messageID.match(/^([0-9]+)$/)) {
@@ -128,8 +135,13 @@ var endpointsAPI  = function(app, database, rootDir) {
             return res.status(400).send();
         }
 
+        /**
+        * [Callback function for confirmation of message retrieval.]
+        * Params: Error flag from database, the contents of the record fetched.
+        * @return {[HTTP 200 on success, HTTP 404 on no MessageID found.]}
+        */
         function messageFetchCallback(err, messageRecord) {
-            if(err || ! messageRecord.Contents) {
+            if(err || ! messageRecord) {
                 return res.status(404).send("Could not find a message with that MessageID.");
             } else {
                 return res.status(200).send(palindromeModule.isPalindrome(messageRecord.Contents));
